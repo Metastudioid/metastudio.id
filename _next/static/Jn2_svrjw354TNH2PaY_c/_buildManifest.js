b1aab04dbc6eaea7bf2d8d2d9025832dd66fdcf5
purwnt <purwanto1337@gmail.com>
2022-01-21 10:37:32 +0000

    Deploying to gh-pages from @ Metastudioid/metastudio.id@b3d38cff7843e15bde027e523270d5d52e588b44 🚀

---
+++ _next/static/Jn2_svrjw354TNH2PaY_c/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/588-fc74f38078de1c56.js","static/chunks/pages/index-e6c901328a3aec85.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
